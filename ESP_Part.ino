8cd6c71b1f04e64166ed2e8a31c68853a4eb17dd
Elsompretos <62136818+elsompretos@users.noreply.github.com>
2020-06-29 12:01:23 +0700

    Update ESP_Part.ino
    Menambahkan proses file json

--- ESP_Part.ino
+++ ESP_Part.ino
@@ -8,6 +8,10 @@ KR=02-02-02-02-02-02-02-02
 
  */
 
+#include <SPI.h>
+#include "FS.h" 
+#include <ArduinoJson.h>
+
 #include <Wire.h>
 
 #include <ESP8266WiFi.h>
@@ -31,8 +35,46 @@ String kirim;
 String bulan, tanggal, tahun, jam, menit, hijri, cerah, volume, adzan, iqosubuh, iqodzuhur, iqoashar, iqomaghrib, iqoisya;
 String korimsak, korsubuh, korterbit, kordhuha, kordzuhur, korashar, kormaghrib, korisya;
 
+//==== JSON ====
+const char *fileadjhijri = "/adjhijrii.json";
+int adjhijr, adjcerah, adjvolume;
+
+const char *fileiqomah = "/iqomah.json";
+int lamaadzan, lamaimsak, iqomahsubuh, iqomahdzuhur, iqomahashar,iqomahmaghrib, iqomahisya;
+
+const char *filemasjid = "/masjidinfo.json";
+char namamasjid[80], info1[80];
+
+const char *filelatilong = "/latilong.json";
+float latitude, longitude; int zonawaktu;
+
+const char *filekoreksi = "/koreksi.json";
+// int koreimsak, koresubuh, koreterbit, koredhuha, koredzuhur, koreashar, koremaghrib, koreisya;
+int b_a[8];
+
+//==== XML ====
+String XML;
+String XML_IQOMAH;
+String XML_MASJID;
+String XML_KOORDINAT;
+String XML_KOREKSI;
+
+
+
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+
+   // =====================
+  // ==== FILE SYSTEM ====
+  // =====================
+  //  LittleFS.begin();
+  SPIFFS.begin();
+  loadHijriConfig(fileadjhijri);
+  loadLatiLongConfig(filelatilong);
+  loadMasjidConfig(filemasjid);
+  loadIqomahConfig(fileiqomah); 
+  loadKoreksi(filekoreksi);
   
   // ==============
   // ==== WIFI ====
@@ -119,6 +161,18 @@ void setup() {
     
   });
   
+  server.on("/xmlwaktu", handleXMLWaktu); // http://192.168.4.1/xmlwaktu
+  server.on("/xmlkoordinat", handleXMLKoordinat); // http://192.168.4.1/xmlkoordinat
+  server.on("/xmlinfomasjid", handleXMLInfoMasjid); // http://192.168.4.1/xmlinfomasjid
+  server.on("/xmliqomah", handleXMLIqomah); // http://192.168.4.1/xmliqomah
+  server.on("/xmlkoreksi", handleXMLKoreksi); // http://192.168.4.1/xmlkoreksi
+
+  server.on("/simpanWaktu", HTTP_POST, handleSimpanWaktu);
+  server.on("/simpanKoordinat", HTTP_POST, handleSimpanKoordinat);
+  server.on("/simpanNamamasjid", HTTP_POST, handleSimpaNamamasjid);
+  server.on("/simpanIqomah", HTTP_POST, handleSimpanIqomah);
+  server.on("/simpanKoreksi", HTTP_POST, handleSimpanKoreksi);
+  
   // ================
   // ==== SERVER ====
   // ================
@@ -132,3 +186,645 @@ void setup() {
 void loop() {
   server.handleClient();
 }
+
+void loadHijriConfig(const char *fileadjhijri){     // Load Penyesuaian Tanggal Hijriyah //
+  File configFilehijr = SPIFFS.open(fileadjhijri, "r");
+  
+  if (!configFilehijr) {
+    Serial.println("Failed to open config file");
+    makeHijriConfig();
+    Serial.println("Sistem restart...");
+    ESP.restart();
+  }
+
+  size_t size = configFilehijr.size();
+  std::unique_ptr<char[]> buf(new char[size]);
+  configFilehijr.readBytes(buf.get(), size);
+
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, buf.get());
+
+  if (error) {
+    Serial.println("Gagal parse sethijri");
+    return;
+  }
+  
+  adjhijr = doc["adjhijr"];
+  adjcerah = doc["adjcerah"];
+  adjvolume = doc["adjvolume"];
+
+  Serial.println("=========================================");
+  Serial.print("Adj Hijriyah : "); Serial.println(adjhijr);
+  Serial.print("Adj Kecerahan : "); Serial.println(adjcerah);
+  Serial.print("Adj Volume : "); Serial.println(adjvolume);
+  
+
+  configFilehijr.close();
+  
+}
+void loadLatiLongConfig(const char *filelatilong){  //         Load Koordinat            //
+  File configLatiLong = SPIFFS.open(filelatilong, "r");
+  
+  if (!configLatiLong) {
+    Serial.println("Gagal membuka pengaturan Koordinat");
+    makeLatiLongConfig();
+    Serial.println("Sistem restart...");
+    ESP.restart();
+  }
+
+  size_t size = configLatiLong.size();
+  std::unique_ptr<char[]> buf(new char[size]);
+  configLatiLong.readBytes(buf.get(), size);
+
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, buf.get());
+
+  if (error) {
+    Serial.println("Gagal parse pengaturan Koordinat");
+    return;
+  }
+
+  latitude = doc["latitude"];
+  longitude = doc["longitude"];
+  zonawaktu = doc["zonawaktu"];   
+  
+
+  Serial.println("=========================================");
+  Serial.print("latitude : "); Serial.println(latitude);
+  Serial.print("longitude : "); Serial.println(longitude);
+  Serial.print("zonawaktu : "); Serial.println(zonawaktu);
+  
+  configLatiLong.close();
+}
+void loadMasjidConfig(const char *filemasjid){      //     Load Nama Masjid dan Info     //
+  File configMasjid = SPIFFS.open(filemasjid, "r");
+  
+  if (!configMasjid) {
+    Serial.println("Gagal membuka pengaturan Masjid dan Info");
+    makeMasjidConfig();
+    Serial.println("Sistem restart...");
+    ESP.restart();
+  }
+
+  size_t size = configMasjid.size();
+  std::unique_ptr<char[]> buf(new char[size]);
+  configMasjid.readBytes(buf.get(), size);
+
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, buf.get());
+
+  if (error) {
+    Serial.println("Gagal parse pengaturan Masjid dan Info");
+    return;
+  }
+
+  strlcpy(namamasjid, doc["namamasjid"] | "", sizeof(namamasjid));  
+  strlcpy(info1, doc["info1"] | "", sizeof(info1));  
+//  strlcpy(info2, doc["info2"] | "", sizeof(info2));  
+//  strlcpy(info3, doc["info3"] | "", sizeof(info3));    
+  
+
+  Serial.println("=========================================");
+  Serial.print("nama masjid : "); Serial.println(namamasjid);
+  Serial.print("info1 : "); Serial.println(info1);
+//  Serial.print("info2 : "); Serial.println(info2);
+//  Serial.print("info3 : "); Serial.println(info3);
+
+
+  configMasjid.close();
+}
+void loadIqomahConfig(const char *fileiqomah){      //     Load Lama Adzan dan Iqomah    //
+  File configFileIqomah = SPIFFS.open(fileiqomah, "r");
+  
+  if (!configFileIqomah) {
+    Serial.println("Failed to open config file");
+    makeIqomahConfig();
+    Serial.println("Sistem restart...");
+    ESP.restart();
+  }
+
+  size_t size = configFileIqomah.size();
+  std::unique_ptr<char[]> buf(new char[size]);
+  configFileIqomah.readBytes(buf.get(), size);
+
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, buf.get());
+
+  if (error) {
+    Serial.println("Gagal parse fileiqomah");
+    return;
+  }
+
+  lamaadzan = doc["lamaadzan"];
+  lamaimsak = doc["lamaimsak"];
+  iqomahsubuh = doc["iqomahsubuh"];
+  iqomahdzuhur = doc["iqomahdzuhur"];
+  iqomahashar = doc["iqomahashar"];
+  iqomahmaghrib = doc["iqomahmaghrib"];
+  iqomahisya = doc["iqomahisya"];
+
+  Serial.println("=========================================");
+  Serial.print("lama adzan : "); Serial.println(lamaadzan);
+  Serial.print("lama imsak : "); Serial.println(lamaimsak);
+  Serial.print("iqomah subuh : "); Serial.println(iqomahsubuh);
+  Serial.print("iqomah dzuhur : "); Serial.println(iqomahdzuhur);
+  Serial.print("iqomah ashar : "); Serial.println(iqomahashar);
+  Serial.print("iqomah maghrib : "); Serial.println(iqomahmaghrib);
+  Serial.print("iqomah isya : "); Serial.println(iqomahisya);
+
+  configFileIqomah.close();
+  
+}
+void loadKoreksi(const char *filekoreksi){          //          Load Koreksi             //
+  File configKoreksi = SPIFFS.open(filekoreksi, "r");
+  
+  if (!configKoreksi) {
+    Serial.println("Gagal membuka pengaturan Koreksi Waktu");
+    makeKoreksi();
+    Serial.println("Sistem restart...");
+    ESP.restart();
+  }
+
+  size_t size = configKoreksi.size();
+  std::unique_ptr<char[]> buf(new char[size]);
+  configKoreksi.readBytes(buf.get(), size);
+
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, buf.get());
+
+  if (error) {
+    Serial.println("Gagal parse pengaturan Koreksi Waktu");
+    return;
+  }
+
+  b_a[0] = doc["korimsak"];
+  b_a[1] = doc["korsubuh"];
+  b_a[2] = doc["korterbit"]; 
+  b_a[3] = doc["kordhuha"];
+  b_a[4] = doc["kordzuhur"];
+  b_a[5] = doc["korashar"];
+  b_a[6] = doc["kormaghrib"];
+  b_a[7] = doc["korisya"];  
+
+     // TEST AJA //
+     // b_a[2] = b_a[2] - 2;
+     // b_a[3] = b_a[3] - 1;
+     
+  Serial.println("=========================================");
+  Serial.print("kor imsak : "); Serial.println(b_a[0]);
+  Serial.print("kor subuh : "); Serial.println(b_a[1]);
+  Serial.print("kor terbit : "); Serial.println(b_a[2]);
+  Serial.print("kor dhuha : "); Serial.println(b_a[3]);
+  Serial.print("kor dzuhur : "); Serial.println(b_a[4]);
+  Serial.print("kor ashar : "); Serial.println(b_a[5]);
+  Serial.print("kor maghrib : "); Serial.println(b_a[6]);
+  Serial.print("kor isya : "); Serial.println(b_a[7]);
+  
+  configKoreksi.close();
+}
+
+void makeHijriConfig(){                             //        Kalau Gak Ada Buat...      //
+  String dataawal = "{\"adjhijr\":1,\"adjcerah\":10,\"adjvolume\":100}";
+
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, dataawal);
+
+  File configFileAdjHijri = SPIFFS.open(fileadjhijri, "w");
+  if (!configFileAdjHijri) {
+    Serial.println("Gagal membuat file adjhijrii.json untuk ditulis mungkin partisi belum dibuat");
+    return;
+  }
+  
+  serializeJson(doc, configFileAdjHijri);
+
+  if (error) {
+    
+    Serial.print(F("deserializeJson() gagal kode sebagai berikut: "));
+    Serial.println(error.c_str());
+    return;
+    
+  } else {
+    
+    configFileAdjHijri.close();
+    Serial.println("Berhasil membuat file configjws.json");
+  
+  }   
+}
+void makeLatiLongConfig(){ 
+   
+  String dataawalkoordinat = "{\"latitude\": \"-6.21462\",\"longitude\": \"106.84513\",\"zonawaktu\": \"7\"}";
+
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, dataawalkoordinat);
+
+  File makeFileKoordinat = SPIFFS.open(filelatilong, "w");
+  if (!makeFileKoordinat) {
+    Serial.println("Gagal membuat file latilong.json untuk ditulis mungkin partisi belum dibuat");
+    return;
+  }
+  
+  serializeJson(doc, makeFileKoordinat);
+
+  if (error) {
+    
+    Serial.print(F("deserializeJson() gagal kode sebagai berikut: "));
+    Serial.println(error.c_str());
+    return;
+    
+  } else {
+    
+    makeFileKoordinat.close();
+    Serial.println("Berhasil membuat file iqomah.json");
+  
+  }   
+}
+void makeMasjidConfig(){  
+    
+  //String dataawalmasjid = "{\"namamasjid\": \"Masjid Raya Akbar Maulana\",\"info1\": \"Info1\",\"info2\": \"Info2\",\"info3\": \"Info3\"}";
+  String dataawalmasjid = "{\"namamasjid\": \"Masjid Raya Akbar Maulana\",\"info1\": \"Info1\"}";
+
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, dataawalmasjid);
+
+  File makeFileMasjid = SPIFFS.open(filemasjid, "w");
+  if (!makeFileMasjid) {
+    Serial.println("Gagal membuat file masjidinfo.json untuk ditulis mungkin partisi belum dibuat");
+    return;
+  }
+  
+  serializeJson(doc, makeFileMasjid);
+
+  if (error) {
+    
+    Serial.print(F("deserializeJson() gagal kode sebagai berikut: "));
+    Serial.println(error.c_str());
+    return;
+    
+  } else {
+    
+    makeFileMasjid.close();
+    Serial.println("Berhasil membuat file iqomah.json");
+  
+  }   
+}
+void makeIqomahConfig(){
+      
+  String dataawaliqomah = "{\"lamaadzan\": \"1\",\"lamaimsak\": \"1\",\"iqomahsubuh\": \"1\",\"iqomahdzuhur\": \"1\",\"iqomahashar\": \"1\",\"iqomahmaghrib\": \"1\",\"iqomahisya\": \"1\"}";
+
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, dataawaliqomah);
+
+  File makeFileIqomah = SPIFFS.open(fileiqomah, "w");
+  if (!makeFileIqomah) {
+    Serial.println("Gagal membuat file iqomah.json untuk ditulis mungkin partisi belum dibuat");
+    return;
+  }
+  
+  serializeJson(doc, makeFileIqomah);
+
+  if (error) {
+    
+    Serial.print(F("deserializeJson() gagal kode sebagai berikut: "));
+    Serial.println(error.c_str());
+    return;
+    
+  } else {
+    
+    makeFileIqomah.close();
+    Serial.println("Berhasil membuat file iqomah.json");
+  
+  }   
+}
+void makeKoreksi(){ 
+   
+  String dataawalkoreksi = "{\"korimsak\": \"2\",\"korsubuh\": \"2\",\"korterbit\": \"0\",\"kordhuha\": \"0\",\"kordzuhur\": \"2\",\"korashar\": \"2\",\"kormaghrib\": \"2\",\"korisya\": \"2\"}";
+
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, dataawalkoreksi);
+
+  File makeFileKoreksi = SPIFFS.open(filekoreksi, "w");
+  if (!makeFileKoreksi) {
+    Serial.println("Gagal membuat file koreksi.json untuk ditulis mungkin partisi belum dibuat");
+    return;
+  }
+  
+  serializeJson(doc, makeFileKoreksi);
+
+  if (error) {
+    
+    Serial.print(F("deserializeJson() gagal kode sebagai berikut: "));
+    Serial.println(error.c_str());
+    return;
+    
+  } else {
+    
+    makeFileKoreksi.close();
+    Serial.println("Berhasil membuat file koreksi.json");
+  
+  }   
+}
+
+void XMLWaktu(){
+  XML="<?xml version='1.0'?>";
+  XML+="<t>";
+    XML+="<Hijriyah>";
+    XML+= adjhijr;
+    XML+="</Hijriyah>"; 
+
+    XML+="<Kecerahan>";
+    XML+= adjcerah;
+    XML+="</Kecerahan>"; 
+
+    XML+="<Volume>";
+    XML+= adjvolume;
+    XML+="</Volume>"; 
+  XML+="</t>"; 
+}
+void handleXMLWaktu(){
+  XMLWaktu();
+  server.send(200,"text/xml",XML);
+}
+void XMLKoordinat(){
+
+  XML_KOORDINAT="<?xml version='1.0'?>";
+  XML_KOORDINAT+="<t>";
+  
+    XML_KOORDINAT+="<Latitude>";
+    XML_KOORDINAT+= latitude;
+    XML_KOORDINAT+="</Latitude>"; 
+
+    XML_KOORDINAT+="<Longitude>";
+    XML_KOORDINAT+= longitude;
+    XML_KOORDINAT+="</Longitude>"; 
+
+    XML_KOORDINAT+="<Timezone>";
+    XML_KOORDINAT+= zonawaktu;
+    XML_KOORDINAT+="</Timezone>"; 
+    
+  XML_KOORDINAT+="</t>"; 
+}
+void handleXMLKoordinat(){
+  XMLKoordinat();
+  server.send(200,"text/xml",XML_KOORDINAT);
+}
+void XMLInfoMasjid(){
+
+  XML_MASJID="<?xml version='1.0'?>";
+  XML_MASJID+="<t>";
+  
+    XML_MASJID+="<NamaMasjid>";
+    XML_MASJID+= namamasjid;
+    XML_MASJID+="</NamaMasjid>"; 
+
+    XML_MASJID+="<InfoSatu>";
+    XML_MASJID+= info1;
+    XML_MASJID+="</InfoSatu>"; 
+
+    
+  XML_MASJID+="</t>"; 
+}
+void handleXMLInfoMasjid(){
+  XMLInfoMasjid();
+  server.send(200,"text/xml", XML_MASJID);
+}
+void XMLIqomah(){
+
+  XML_IQOMAH="<?xml version='1.0'?>";
+  XML_IQOMAH+="<t>";
+  
+    XML_IQOMAH+="<LamZan>";
+    XML_IQOMAH+= lamaadzan;
+    XML_IQOMAH+="</LamZan>"; 
+
+    XML_IQOMAH+="<LamSak>";
+    XML_IQOMAH+= lamaimsak;
+    XML_IQOMAH+="</LamSak>"; 
+
+    XML_IQOMAH+="<ISub>";
+    XML_IQOMAH+= iqomahsubuh;
+    XML_IQOMAH+="</ISub>"; 
+
+    XML_IQOMAH+="<IDzu>";
+    XML_IQOMAH+= iqomahdzuhur;
+    XML_IQOMAH+="</IDzu>";
+
+    XML_IQOMAH+="<IAshr>";
+    XML_IQOMAH+= iqomahashar;
+    XML_IQOMAH+="</IAshr>"; 
+
+    XML_IQOMAH+="<IMgr>";
+    XML_IQOMAH+= iqomahmaghrib ;
+    XML_IQOMAH+="</IMgr>"; 
+
+    XML_IQOMAH+="<IIsy>";
+    XML_IQOMAH+= iqomahisya;
+    XML_IQOMAH+="</IIsy>"; 
+    
+  XML_IQOMAH+="</t>"; 
+}
+void handleXMLIqomah(){
+  XMLIqomah();
+  server.send(200,"text/xml", XML_IQOMAH);
+}
+void XMLKoreksi(){
+
+  XML_KOREKSI="<?xml version='1.0'?>";
+  XML_KOREKSI+="<t>";
+  
+    XML_KOREKSI+="<KorImsak>";
+    XML_KOREKSI+= b_a[0];
+    XML_KOREKSI+="</KorImsak>"; 
+
+    XML_KOREKSI+="<KorSubuh>";
+    XML_KOREKSI+= b_a[1];
+    XML_KOREKSI+="</KorSubuh>"; 
+
+    XML_KOREKSI+="<KorTerbit>";
+    XML_KOREKSI+= b_a[2];
+    XML_KOREKSI+="</KorTerbit>"; 
+
+    XML_KOREKSI+="<KorDhuha>";
+    XML_KOREKSI+= b_a[3];
+    XML_KOREKSI+="</KorDhuha>";
+
+    XML_KOREKSI+="<KorDzuhur>";
+    XML_KOREKSI+= b_a[4];
+    XML_KOREKSI+="</KorDzuhur>"; 
+
+    XML_KOREKSI+="<KorAshar>";
+    XML_KOREKSI+= b_a[5];
+    XML_KOREKSI+="</KorAshar>"; 
+
+    XML_KOREKSI+="<KorMaghrib>";
+    XML_KOREKSI+= b_a[6];
+    XML_KOREKSI+="</KorMaghrib>"; 
+
+    XML_KOREKSI+="<KorIsya>";
+    XML_KOREKSI+= b_a[7];
+    XML_KOREKSI+="</KorIsya>"; 
+    
+  XML_KOREKSI+="</t>"; 
+}
+void handleXMLKoreksi(){
+  XMLKoreksi();
+  server.send(200,"text/xml", XML_KOREKSI);
+}
+
+void handleSimpanWaktu() {
+
+  String datainfoWaktu = server.arg("plain");
+  
+  Serial.println(datainfoWaktu);
+  
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, datainfoWaktu);
+
+  File configFileWaktu = SPIFFS.open(fileadjhijri, "w");
+
+  if (!configFileWaktu) {
+    Serial.println("Gagal membuka File info untuk ditulis");
+    return;
+  }
+
+  serializeJson(doc, configFileWaktu);
+
+  if (error) {
+    
+    Serial.print(F("deserializeJson() gagal kode sebagai berikut: "));
+    Serial.println(error.c_str());
+    return;
+    
+  } else {
+    
+    configFileWaktu.close();
+    Serial.println("Berhasil mengubah configFileJws");
+
+    loadHijriConfig(fileadjhijri);
+  }   
+}
+void handleSimpanKoordinat() {
+
+  String datainfoKoordinat = server.arg("plain");
+  
+  Serial.println(datainfoKoordinat);
+  
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, datainfoKoordinat);
+
+  File configFileKoordinat = SPIFFS.open(filelatilong, "w");
+
+  if (!configFileKoordinat) {
+    Serial.println("Gagal membuka File info untuk ditulis");
+    return;
+  }
+
+  serializeJson(doc, configFileKoordinat);
+
+  if (error) {
+    
+    Serial.print(F("deserializeJson() gagal kode sebagai berikut: "));
+    Serial.println(error.c_str());
+    return;
+    
+  } else {
+    
+    configFileKoordinat.close();
+    Serial.println("Berhasil mengubah configFileJws");
+
+    loadLatiLongConfig(filelatilong);
+  }   
+}
+void handleSimpaNamamasjid(){
+   String datainfoNamamasjid = server.arg("plain");
+  
+  Serial.println(datainfoNamamasjid);
+  
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, datainfoNamamasjid);
+
+  File configFileNamamasjid = SPIFFS.open(filemasjid, "w");
+
+  if (!configFileNamamasjid) {
+    Serial.println("Gagal membuka File info untuk ditulis");
+    return;
+  }
+
+  serializeJson(doc, configFileNamamasjid);
+
+  if (error) {
+    
+    Serial.print(F("deserializeJson() gagal kode sebagai berikut: "));
+    Serial.println(error.c_str());
+    return;
+    
+  } else {
+    
+    configFileNamamasjid.close();
+    Serial.println("Berhasil mengubah configFileJws");
+
+    loadMasjidConfig(filemasjid);
+  }   
+}
+void handleSimpanIqomah(){
+String datainfoIqomah = server.arg("plain");
+  
+  Serial.println(datainfoIqomah);
+  
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, datainfoIqomah);
+
+  File configFileIqomah = SPIFFS.open(fileiqomah, "w");
+
+  if (!configFileIqomah) {
+    Serial.println("Gagal membuka File info untuk ditulis");
+    return;
+  }
+
+  serializeJson(doc, configFileIqomah);
+
+  if (error) {
+    
+    Serial.print(F("deserializeJson() gagal kode sebagai berikut: "));
+    Serial.println(error.c_str());
+    return;
+    
+  } else {
+    
+    configFileIqomah.close();
+    Serial.println("Berhasil mengubah configFileJws");
+
+    loadIqomahConfig(fileiqomah); 
+  }   
+}
+void handleSimpanKoreksi(){
+  String datainfoKoreksi = server.arg("plain");
+  
+  Serial.println(datainfoKoreksi);
+  
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, datainfoKoreksi);
+
+  File configFileKoreksi = SPIFFS.open(filekoreksi, "w");
+
+  if (!configFileKoreksi) {
+    Serial.println("Gagal membuka File info untuk ditulis");
+    return;
+  }
+
+  serializeJson(doc, configFileKoreksi);
+
+  if (error) {
+    
+    Serial.print(F("deserializeJson() gagal kode sebagai berikut: "));
+    Serial.println(error.c_str());
+    return;
+    
+  } else {
+    
+    configFileKoreksi.close();
+    Serial.println("Berhasil mengubah configFileJws");
+
+    loadKoreksi(filekoreksi);
+  }   
+}
+
