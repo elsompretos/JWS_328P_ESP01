c7c5be8f7aefb0834f5be1926c1dbf50320edd89
Elsompretos <62136818+elsompretos@users.noreply.github.com>
2021-03-09 12:53:05 +0700

    Pisahkan Adjust Volume

---
+++ VERSI_4/bagian_uno/dengan_tampil _haribesar/tulis_eeprom/tulis_eeprom.ino
@@ -0,0 +1,163 @@
+#include <EEPROM.h>
+//double  blatitude, blongitude, btz;
+//int badj_subuh, badj_dzuhur, badj_ashar, badj_maghrib, badj_isya, badj_imsak, badj_terbit, badj_dhuha;
+//int btpl_subuh, btpl_dzuhur, btpl_ashar, btpl_maghrib, btpl_isya, btpl_imsak, btpl_terbit, btpl_dhuha;
+//int biqm_subuh, biqm_dzuhur, biqm_ashar, biqm_maghrib, biqm_isya, blam_zan, blama_imsak;
+//int bsesuaikan_tgl_hijriyah, bsesuaikan_bright, bsesuaikan_volume;
+//  
+//
+//String nama;
+//String info1, info2, info3, info4;
+
+
+void setup() {
+
+  Serial.begin(9600);
+
+  //  LATITUDE DAN LONGITUDE
+  //  ======================
+
+  double latitude = -6.21462;
+  EEPROM.put(0, latitude);
+  double longitude = 106.84513;
+  EEPROM.put(5, longitude);
+  double tz = 7;
+  EEPROM.put(10, tz);
+
+//  EEPROM.get(0, blatitude);
+//  Serial.print("0 Latitude : "); Serial.println(blatitude);
+//  EEPROM.get(5, blongitude);
+//  Serial.print("5 Longitude : "); Serial.println(blongitude);
+//  EEPROM.get(10, btz);
+//  Serial.print("10 Timezone : "); Serial.println(btz); Serial.println();
+ 
+  //  ADJUST WAKTU SHOLAT
+  //  ===================
+
+  int adj_subuh = 2; EEPROM.put(15, adj_subuh);
+  int adj_dzuhur = 2; EEPROM.put(17, adj_dzuhur);
+  int adj_ashar = 2; EEPROM.put(19, adj_ashar);
+  int adj_maghrib = 2; EEPROM.put(21, adj_maghrib);
+  int adj_isya = 2; EEPROM.put(23, adj_isya);
+  int adj_imsak = 0; EEPROM.put(25, adj_imsak);
+  int adj_terbit = 0; EEPROM.put(27, adj_terbit);
+  int adj_dhuha = 2; EEPROM.put(29, adj_dhuha);
+  
+//  EEPROM.get(15, badj_subuh); Serial.print("15 adj_subuh : "); Serial.println(badj_subuh);
+//  EEPROM.get(17, badj_dzuhur); Serial.print("17 adj_dzuhur : "); Serial.println(badj_dzuhur);
+//  EEPROM.get(19, badj_ashar); Serial.print("19 adj_ashar : "); Serial.println(badj_ashar);
+//  EEPROM.get(21, badj_maghrib); Serial.print("21 adj_maghrib : "); Serial.println(badj_maghrib); 
+//  EEPROM.get(23, badj_isya); Serial.print("23 adj_isya : "); Serial.println(badj_isya);
+//  EEPROM.get(25, badj_imsak); Serial.print("25 adj_imsak : "); Serial.println(badj_imsak);
+//  EEPROM.get(27, badj_terbit); Serial.print("27 adj_terbit : "); Serial.println(badj_terbit);
+//  EEPROM.get(29, badj_dhuha); Serial.print("29 adj_dhuha : "); Serial.println(badj_dhuha);
+//  Serial.println();
+
+  //  TAMPILKAN WAKTU SHOLAT
+  //  ======================
+
+  int tpl_subuh = 1; EEPROM.put(32, tpl_subuh);
+  int tpl_dzuhur = 1; EEPROM.put(34, tpl_dzuhur);
+  int tpl_ashar = 1; EEPROM.put(36, tpl_ashar);
+  int tpl_maghrib = 1; EEPROM.put(38, tpl_maghrib);
+  int tpl_isya = 1; EEPROM.put(40, tpl_isya);
+  int tpl_imsak = 1; EEPROM.put(42, tpl_imsak);
+  int tpl_terbit = 1; EEPROM.put(44, tpl_terbit);
+  int tpl_dhuha = 1; EEPROM.put(46, tpl_dhuha);
+
+//  EEPROM.get(31, btpl_subuh); Serial.print("31 tpl_subuh : "); Serial.println(btpl_subuh);
+//  EEPROM.get(33, btpl_dzuhur); Serial.print("33 tpl_dzuhur : "); Serial.println(btpl_dzuhur);
+//  EEPROM.get(35, btpl_ashar); Serial.print("35 tpl_ashar : "); Serial.println(btpl_ashar);
+//  EEPROM.get(37, btpl_maghrib); Serial.print("37 tpl_maghrib : "); Serial.println(btpl_maghrib); 
+//  EEPROM.get(39, btpl_isya); Serial.print("39 tpl_isya : "); Serial.println(btpl_isya);
+//  EEPROM.get(41, btpl_imsak); Serial.print("41 tpl_imsak : "); Serial.println(btpl_imsak);
+//  EEPROM.get(43, btpl_terbit); Serial.print("43 tpl_terbit : "); Serial.println(btpl_terbit);
+//  EEPROM.get(45, btpl_dhuha); Serial.print("45 tpl_dhuha : "); Serial.println(btpl_dhuha);
+//  Serial.println();
+
+  //  LAMA IQOMAH
+  //  ===========
+
+  int iqm_subuh = 2; EEPROM.put(49, iqm_subuh);
+  int iqm_dzuhur = 2; EEPROM.put(51, iqm_dzuhur);
+  int iqm_ashar = 2; EEPROM.put(53, iqm_ashar);
+  int iqm_maghrib = 2; EEPROM.put(55, iqm_maghrib);
+  int iqm_isya = 2; EEPROM.put(57, iqm_isya);
+  int lama_adzan = 4; EEPROM.put(59, lama_adzan);
+
+//  EEPROM.get(47, biqm_subuh); Serial.print("47 iqm_subuh : "); Serial.println(biqm_subuh); 
+//  EEPROM.get(49, biqm_dzuhur); Serial.print("49 iqm_dzuhur : "); Serial.println(biqm_dzuhur);
+//  EEPROM.get(51, biqm_ashar); Serial.print("51 iqm_ashar : "); Serial.println(biqm_ashar);
+//  EEPROM.get(53, biqm_maghrib); Serial.print("53 iqm_maghrib : "); Serial.println(biqm_maghrib);
+//  EEPROM.get(55, biqm_isya); Serial.print("55 iqm_isya : "); Serial.println(biqm_isya);
+//  EEPROM.get(57, blam_zan); Serial.print("57 lama_adzan : "); Serial.println(blam_zan);
+//  Serial.println();
+
+  //  LAMA IMSAK
+  //  ================
+
+  int lama_imsak = 0; EEPROM.put(61, lama_imsak);
+//  EEPROM.get(57, blama_imsak); Serial.print("57 lama_imsak : "); Serial.println(blama_imsak);
+//  Serial.println(); 
+   
+  //  KOREKSI HIJRIYAH
+  //  ================
+
+  int sesuaikan_tgl_hijriyah = 1; EEPROM.put(63, sesuaikan_tgl_hijriyah); 
+  int sesuaikan_bright = 10; EEPROM.put(65, sesuaikan_bright);
+  int sesuaikan_volume = 20; EEPROM.put(67, sesuaikan_volume);
+
+//  EEPROM.get(59, bsesuaikan_tgl_hijriyah); Serial.print("59 sesuaikan_tgl_hijriyah : "); Serial.println(bsesuaikan_tgl_hijriyah);
+//  EEPROM.get(61, bsesuaikan_bright); Serial.print("61 sesuaikan_bright : "); Serial.println(bsesuaikan_bright);
+//  EEPROM.get(63, bsesuaikan_volume); Serial.print("63 sesuaikan_volume : "); Serial.println(bsesuaikan_volume);
+//  Serial.println();
+
+  writeString(69, "Masjid Raya Akbar Maulana - Ampera Raya");
+  writeString(119, "Jadikan Sabar dan Sholat Sebagai Penolongmu");
+  // writeString(169, "Sedekah Melancarkan Rezekimu");
+  writeString(219, "Subhanallah Walhamdulillah Laailahaillalhah");
+  writeString(269, "Selamat Hari Raya Idul Fitri 1 Syawal 1441 H");
+
+//  nama = readString(65);
+//  Serial.print("65 Nama Masjid  : "); Serial.println(nama);
+//  info1 = readString(115);
+//  Serial.print("115 Informasi 1 : "); Serial.println(info1);
+//  info2 = readString(165);
+//  Serial.print("165 Informasi 2 : "); Serial.println(info2);
+//  info3 = readString(215);
+//  Serial.print("215 Informasi 3 : "); Serial.println(info3);
+//  info4 = readString(265);
+//  Serial.print("265 Informasi 4 : "); Serial.println(info4);
+//  Serial.println();
+
+
+  
+}
+
+void loop() {
+  
+
+}
+void writeString(int address, String data){
+  int stringSize = data.length();
+  
+  for(int i=0;i<stringSize;i++)
+  {
+    EEPROM.write(address+i, data[i]);
+  }
+  EEPROM.write(address + stringSize,'\0');   //Add termination null character
+}
+String readString(int address){
+  char data[100]; //Max 100 Bytes
+  int len=0;
+  unsigned char k;
+  k = EEPROM.read(address);
+  while(k != '\0' && len < 100)   //Read until null character
+  {
+    k = EEPROM.read(address + len);
+    data[len] = k;
+    len++;
+  }
+  data[len]='\0';
+  return String(data);
+}
---
+++ VERSI_4/bagian_uno/dengan_tampil/JWS_328P_ESP01_TAMPIL/tangal_hijriyah.h
@@ -0,0 +1,98 @@
+//char namaBulanHijriyah[][12] = {"Muharram", "Safar", "RabiulAwal", "RabiulAkhir", "JumadilAwal", "JumadilAkhir", "Rajab", "Syaban", "Ramadhan", "Syawal", "Dzulqodah", "Dzulhijjah"};
+// const char namaBulanHijriyah[][14] PROGMEM = {"Muharram", "Safar", "RabiulAwal", "RabiulAkhir", "JumadilAwal", "JumadilAkhir", "Rajab", "Syaban", "Ramadhan", "Syawal", "Dzulqodah", "Dzulhijjah"};
+const char namaBulanHijriyah[][15] PROGMEM = {"Muharram", "Safar", "Rabiul Awal", "Rabiul Akhir", "Jumadil Awal", "Jumadil Akhir", "Rajab", "Syaban", "Ramadhan", "Syawal", "Dzul Qaidah", "Dzul Hijjah"};
+
+String Kuwaiti_algorithm (double tanggalH, double bulanH, double tahunH, int hariH, double adj_hij){
+  
+  tanggalH = tanggalH - adj_hij;
+
+  String res_kalHij;
+  
+  double m = bulanH;
+  double y = tahunH;
+
+  if(m<3) {
+    y -= 1;
+    m += 12;
+  }
+  double a = floor(y/100.);
+  double b = 2 - a + floor(a/4.);
+  
+  if(y<1583) {
+    b = 0;
+  }
+
+  if(y==1582) {
+    if(m>10)  b = -10;
+    if(m==10) {
+      b = 0;
+      if(tanggalH>4) b = -10;
+    }
+  }
+
+  double jd = floor(365.25*(y+4716))+floor(30.6001*(m+1))+tanggalH+b-1524; // CARA 1
+  //uint32_t jd = floor(365.25*(y+4716))+floor(30.6001*(m+1))+tanggalH+b-1524; // CARA 2
+
+  b = 0;
+
+  if(jd>2299160){
+    a = floor((jd-1867216.25)/36524.25);
+    b = 1 + a - floor(a/4.);
+  }
+
+  double bb = jd + b + 1524;
+  double cc = floor((bb - 122.1) / 365.25);
+  double dd = floor(365.25 * cc);
+  double ee = floor((bb - dd) / 30.6001);
+  tanggalH = (bb - dd) - floor(30.6001 * ee);
+  bulanH = ee - 1;
+
+  if(ee>13) {
+    cc += 1;
+    bulanH = ee-13;
+  }
+  tahunH = cc-4716;
+
+  double iyear = 10631./30.;
+  double epochastro = 1948084;
+  double epochcivil = 1948085; //
+
+  double shift1 = 8.01 / 60.;
+
+  double z = jd-epochastro;
+
+  double cyc = floor(z / 10631.);
+  z = z - 10631 * cyc;
+  double j = floor((z - shift1) / iyear);
+  double iy = 30 * cyc + j;
+  z = z - floor(j * iyear + shift1);
+  double im = floor((z + 28.5001) / 29.5);
+  
+  if (im == 13){
+    im = 12;
+  }
+
+  double id = z - floor(29.5001 * im - 29);
+
+//String nm_hr_hjr;
+//switch (hariH+1) { // CARA 1
+//case 1: nm_hr_hjr = "Ahad"; break;
+//case 2: nm_hr_hjr = "Itsnayn"; break;
+//case 3: nm_hr_hjr = "Tsalatsa"; break;
+//case 4: nm_hr_hjr = "Arbaa"; break;
+//case 5: nm_hr_hjr = "Khamsatun"; break;
+//case 6: nm_hr_hjr = "Jumuah"; break;
+//case 7: nm_hr_hjr = "Sabat"; break;
+//}
+
+// Bulan Hijriyah
+char MonthName[12];
+memset(MonthName, 0, 12);
+strcpy_P(MonthName, namaBulanHijriyah[int(im)-1]);
+
+res_kalHij =  String(int(id)) + " " + MonthName + " " + String(int(iy)) + " H";
+// res_kalHij =  nm_hr_hjr + ", " + String(int(id)) + " " + MonthName + " " + String(int(iy)) + " H";
+
+return res_kalHij;
+  
+}
